3715bfd6b21b134d192c4c4f5631fa854f6146d8
Vadim Zelenko <wolf_vadim@hotmail.com>
2020-08-02 15:54:20 +0300

    06-Assert: assertion error

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -347,8 +347,40 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testAmountOfEmptySearch() {
 
-    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia input'",
+                5
+        );
+
+        String search_line = "sfdsdfsdfsdfs";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                search_line,
+                "Cannot find search input",
+                5
+        );
+
+        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        String empty_result_label = "//*[@text='No results found']";
+
+        waitForElementPresent(
+                By.xpath(empty_result_label),
+                "Cannot find empty result label by the request " + search_line,
+                15
+        );
+
+        assertElementNotPresent(
+                By.xpath(search_result_locator),
+                "We've found same results by request " + search_line
+        );
+    }
+
+
+        private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
 
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -462,6 +494,17 @@ public class FirstTest {
 
         List elements = driver.findElements(by);
         return elements.size();
+    }
+
+    private void assertElementNotPresent(By by, String error_message) {
+
+        int amountOfElements = getAmountOfElements(by);
+        if (amountOfElements > 0) {
+            String default_message = "An element '" + by.toString() + "' supposed to be not present";
+            throw new AssertionError(default_message + " " + error_message);
+        }
+
+
     }
 
 
